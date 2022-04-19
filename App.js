b10562ea34ae9a27e1079d2a7829eb946b0c6384
nilesh3535 <74583024+nilesh3535@users.noreply.github.com>
2022-04-19 15:15:30 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,116 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  ActivityIndicator,
+  LogBox,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
+import { checkConnected } from "./functions";
+
+import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import RegisterWms from "./screens/RegisterWms";
+import RegisterFlash from "./screens/RegisterFlash";
+import MyProfile from "./screens/MyProfile";
+const Stack = createStackNavigator();
+LogBox.ignoreLogs([
+  "[react-native-gesture-handler] Seems like you're using an old API with gesture components, check out new Gestures system!",
+]);
 export default function App() {
-  return (
+  const [connectStatus, setConnectStatus] = useState(false);
+  useEffect(() => {
+    checkConnected().then((res) => {
+      setConnectStatus(res);
+    });
+  });
+  checkConnectedAgain = () => {
+    checkConnected().then((res) => {
+      setConnectStatus(res);
+    });
+  };
+  return connectStatus ? (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerMode: "screen",
+          headerTintColor: "white",
+          headerStyle: { backgroundColor: "#80daeb" },
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Register"
+          component={RegisterScreen}
+          options={{
+            title: "Society Register",
+            headerStyle: {
+              backgroundColor: "#f0ad4e",
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          }}
+        />
+        <Stack.Screen
+          name="RegisterWms"
+          component={RegisterWms}
+          options={{
+            title: "Enter  Details",
+            headerStyle: {
+              backgroundColor: "#f0ad4e",
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          }}
+        />
+        <Stack.Screen
+          name="RegisterFlash"
+          component={RegisterFlash}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="MyProfile"
+          component={MyProfile}
+          options={{
+            title: "My Profile",
+            headerStyle: {
+              backgroundColor: "#80daeb",
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  ) : (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar backgroundColor="#5abcd8" style="light" />
+      <Button title="Reload" onPress={checkConnectedAgain} />
+      <ActivityIndicator size="small" color="#0000ff" />
+      <Text>No Internet Connection</Text>
     </View>
   );
 }
@@ -13,8 +118,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
